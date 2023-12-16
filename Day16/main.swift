1c4f87488a13322252e3adebece24275536d1932
Will McGinty <mcgintw@gmail.com>
2023-12-16 01:35:36 -0600

    Clean up input issues, solution

--- Day16/main.swift
+++ Day16/main.swift
@@ -28,72 +28,22 @@ let lineParser = Many(1...) { Content.parser(of: Substring.self) } terminator: {
     }
 }
 let inputParser = Many { lineParser }.map(Grid.init)
-let grid = try inputParser.parse(String.input)
 
-struct BeamMovement: Hashable {
+struct Beam: Hashable {
     let coordinate: Coordinate
     let direction: Coordinate.Direction
 }
 
 extension Grid<Content> {
 
-    struct State: Hashable {
-        let previousMovements: Set<BeamMovement>
-        let currentMovement: BeamMovement
-        let visitedCoordinates: Set<Coordinate>
+    func beams(from beam: Beam) -> [Beam] {
+        let coordinate = beam.coordinate
+        let direction = beam.direction
         
-        init(previousMovements: Set<BeamMovement>, currentMovement: BeamMovement) {
-            self.previousMovements = previousMovements
-            self.currentMovement = currentMovement
-            self.visitedCoordinates = Set(previousMovements.map(\.coordinate))
-        }
-    }
-    
-    func beamPath(startingAt starts: Set<BeamMovement>) -> Set<BeamMovement> {
-        var seen: Set<BeamMovement> = []
-        var deque = Deque<State>()
-        
-        for start in starts {
-            let movement = BeamMovement(coordinate: start.coordinate, direction: start.direction)
-            deque.append(.init(previousMovements: [movement], currentMovement: movement))
-        }
-    
-        while let next = deque.popFirst() {
-            if seen.contains(next.currentMovement) {
-                continue
-            }
-            
-            seen.insert(next.currentMovement)
-            let beamOutputs = beamOutput(afterEntering: next.currentMovement.coordinate, moving: next.currentMovement.direction)
-            for output in beamOutputs {
-                if self.contents(at: output.coordinate) != nil {
-                    deque.append(.init(previousMovements: next.previousMovements.union([next.currentMovement]),
-                                       currentMovement: output))
-                }
-            }
-        }
-        
-        return seen
-    }
-    
-    func bestBeamPath(startingAt starts: [BeamMovement]) -> Int {
-        var max = 0
-        for start in starts {
-            let path = beamPath(startingAt: [start])
-            let coordCount = Set(path.map(\.coordinate)).count
-            if coordCount > max {
-                print("found \(coordCount)")
-                max = coordCount
-            }
-        }
-        
-        return max
-    }
-    
-    func beamOutput(afterEntering coordinate: Coordinate, moving direction: Coordinate.Direction) -> Set<BeamMovement> {
         switch self[coordinate] {
         case .empty:
             return [.init(coordinate: coordinate.neighbor(in: direction), direction: direction)]
+            
         case .leftMirror:
             switch direction {
             case .east: return [.init(coordinate: coordinate.neighbor(in: .north), direction: .north)]
@@ -116,6 +66,7 @@ extension Grid<Content> {
                 .init(coordinate: coordinate.neighbor(in: .east), direction: .east),
                 .init(coordinate: coordinate.neighbor(in: .west), direction: .west),
             ]
+            
         case .verticalSplitter:
             guard direction.isEastOrWest else { return [.init(coordinate: coordinate.neighbor(in: direction), direction: direction)] }
             return [
@@ -124,21 +75,50 @@ extension Grid<Content> {
             ]
         }
     }
+    
+    func energizedTiles(startingAt start: Coordinate, direction: Coordinate.Direction) -> Set<Coordinate> {
+        var energized: Set<Coordinate> = []
+        var seen: Set<Beam> = []
+        
+        var deque: Deque<Beam> = [.init(coordinate: start, direction: direction)]
+        while let next = deque.popFirst() {
+            if contents(at: next.coordinate) == nil {
+                continue
+            }
+    
+            energized.insert(next.coordinate)
+            if seen.contains(next) {
+                continue
+            }
+    
+            seen.insert(next)
+            deque.append(contentsOf: beams(from: next))
+        }
+        
+        return energized
+    }
+    
+    func mostEnergizedTiles(fromPossibleStarts starts: [Beam]) -> Int {
+        return starts.map {
+            return grid.energizedTiles(startingAt: $0.coordinate, direction: $0.direction)
+        }.max(by: \.count)?.count ?? 0
+    }
 }
+
+let grid = try inputParser.parse(String.input)
 
 measure(part: .one) { logger in
     /* Part One */
-    let energizedTiles = Set(grid.beamPath(startingAt: [.init(coordinate: .init(x: 0, y: 0), direction: .east)]))
-    let count = Set(energizedTiles.map(\.coordinate)).count
-    return count
+    return grid.energizedTiles(startingAt: .init(x: 0, y: 0), direction: .east).count
 }
 
 measure(part: .two) { logger in
     /* Part Two */
-    let starts: [BeamMovement] = grid.columns(forRow: 0).map { .init(coordinate: .init(row: 0, column: $0), direction: .south) }
-    + grid.columns(forRow: grid.lastRowIndex).map { .init(coordinate: .init(row: grid.lastRowIndex, column: $0), direction: .north) }
-    + grid.rows.map { .init(coordinate: .init(row: $0, column: 0), direction: .east) }
-    + grid.rows.map { .init(coordinate: .init(row: $0, column: grid.lastColumnIndex(forRow: 0)), direction: .west) }
     
-    return grid.bestBeamPath(startingAt: starts)
+    let starts: [Beam] = grid.coordinatesForRow(at: 0).map { Beam(coordinate: $0, direction: .south) }
+    + grid.coordinatesForRow(at: grid.lastRowIndex).map { Beam(coordinate: $0, direction: .north) }
+    + grid.coordinatesForColumn(at: 0).map { Beam(coordinate: $0, direction: .east) }
+    + grid.coordinatesForColumn(at: grid.lastColumnIndex(forRow: 0)).map { Beam(coordinate: $0, direction: .west) }
+        
+    return grid.mostEnergizedTiles(fromPossibleStarts: starts)
 }
