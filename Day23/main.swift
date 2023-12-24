731d55e008792228d8ed2ca4e0c2b86168a6fec5
Will McGinty <mcgintw@gmail.com>
2023-12-24 13:47:49 -0500

    Clean up day 23, part of day 24

--- Day23/main.swift
+++ Day23/main.swift
@@ -26,17 +26,17 @@ let lineParser = Many { Contents.parser(of: Substring.self) }
 let inputParser = Many { lineParser } separator: { Whitespace(1, .vertical) }
 let grid = try Grid<Contents>(contents: inputParser.parse(String.input))
 
-struct DistanceToCoordinate: Hashable {
-    let coordinate: Coordinate
-    let distance: Int
-}
-
 extension Grid<Contents> {
     
     struct State: Hashable {
         let coordinate: Coordinate
         let stepCount: Int
-        let visited: Set<Coordinate>
+        let visited: [Coordinate]
+    }
+    
+    struct EdgeState: Hashable {
+        let coordinate: Coordinate
+        let distance: Int
     }
     
     var start: Coordinate { allCoordinates.first(where: { grid.contents(at: $0) == .path })! }
@@ -64,67 +64,65 @@ extension Grid<Contents> {
         
         return neighbors
     }
-    
-    func navigationGraph(respectingSlopes: Bool = true) -> [Coordinate: [Coordinate: Int]] {
-        var vertices: [Coordinate] = [start]
-        var visited: Set<Coordinate> = []
         
+    func vertices(connectedTo coordinate: Coordinate, respectingSlopes: Bool, movingToward end: Coordinate) -> [Coordinate: Int] {
+        var destinations: [Coordinate: Int] = [:]
+        
+        var deque: Deque<EdgeState> = [.init(coordinate: coordinate, distance: 0)]
+        var seen: Set<Coordinate> = []
+        while let next = deque.popFirst() {
+            if seen.contains(next.coordinate) {
+                continue
+            }
+            seen.insert(next.coordinate)
+            
+            if next.coordinate == end {
+                destinations[next.coordinate] = next.distance
+                continue
+            }
+            
+            let neighbors = neighbors(of: next.coordinate, respectingSlopes: respectingSlopes)
+            if neighbors.count > 2 && next.coordinate != coordinate {
+                destinations[next.coordinate] = next.distance
+                continue
+            }
+            
+            deque.append(contentsOf: neighbors.map { .init(coordinate: $0, distance: next.distance + 1) })
+        }
+        
+        return destinations
+    }
+    
+    func navigationGraph(respectingSlopes: Bool = true, movingToward end: Coordinate) -> [Coordinate: [Coordinate: Int]] {
         var graph: [Coordinate: [Coordinate: Int]] = [:]
-        while let vertex = vertices.popLast() {
+        var vertices: Deque<Coordinate> = [start]
+        var visited: Set<Coordinate> = []
+        while let vertex = vertices.popFirst() {
             if visited.contains(vertex) {
                 continue
             }
-     
-            for nextStep in neighbors(of: vertex, respectingSlopes: respectingSlopes) {
-                var length = 1
-                var prev = vertex
-                var position = nextStep
-                var deadEnd = false
-
-                while true {
-                    let neighbors =  neighbors(of: position, respectingSlopes: respectingSlopes)
-                    if neighbors == [prev] && "<>^v".contains(grid[position].rawValue) {
-                        deadEnd = true
-                        break
-                    }
-                    if neighbors.count != 2 {
-                        break
-                    }
-                    for neighbor in neighbors {
-                        if neighbor != prev {
-                            length += 1
-                            prev = position
-                            position = neighbor
-                            break
-                        }
-                    }
-                }
-
-                if deadEnd {
-                    continue
-                }
-
-                graph[vertex, default: [:]][position] = length
-                vertices.append(position)
-            }
-
+            
+            let destinations = self.vertices(connectedTo: vertex, respectingSlopes: respectingSlopes, movingToward: end)
+            graph[vertex] = destinations
+            
+            vertices.append(contentsOf: destinations.keys)
             visited.insert(vertex)
         }
 
         return graph
     }
         
-    func longestHike(from start: Coordinate, to goal: Coordinate, respectingSlopes: Bool) -> Int? {
-        let graph = navigationGraph(respectingSlopes: respectingSlopes)
+    func longestHike(from start: Coordinate, to end: Coordinate, respectingSlopes: Bool) -> Int? {
+        let graph = navigationGraph(respectingSlopes: respectingSlopes, movingToward: end)
     
         var stepCounts: [Int] = []
         var deque: Deque<State> = [.init(coordinate: start, stepCount: 0, visited: [start])]
         while let next = deque.popLast() {
-            if next.coordinate == goal {
+            if next.coordinate == end {
                 stepCounts.append(next.stepCount)
                 continue
             }
-                       
+                              
             if let distancesToNextVertices = graph[next.coordinate] {
                 for (coordinate, distance) in distancesToNextVertices {
                     if next.visited.contains(coordinate) {
@@ -133,7 +131,7 @@ extension Grid<Contents> {
                     
                     deque.append(.init(coordinate: coordinate,
                                        stepCount: next.stepCount + distance,
-                                       visited: next.visited.union([coordinate])))
+                                       visited: next.visited + [coordinate]))
                 }
             }
         }
@@ -149,42 +147,3 @@ measure(part: .one) {
 measure(part: .two) {
     return grid.longestHike(from: grid.start, to: grid.end, respectingSlopes: false) ?? -1
 }
-
-//var edges: [Coordinate: [(Coordinate, Int)]] = [:]
-//    for coordinate in vertices {
-//        var otherVertices = vertices
-//        otherVertices.remove(coordinate)
-//
-//        var distances: [(Coordinate, Int)] = []
-//        for vertex in otherVertices {
-////            print("Shortest: \(coordinate) to \(vertex):")
-//            if let shortest = grid.shortestPath(from: coordinate, to: vertex, respectingSlopes: true) {
-////                print(grid.description { c, _ in
-////                    return shortest.contains(c) ? "X" : nil
-////                })
-//
-//
-//                distances.append((vertex, shortest.count))
-//            }
-////            print()
-//        }
-////
-//        edges[coordinate] = distances
-//    }
-        
-//    func shortestPath(from coordinate: Coordinate, to: Coordinate, respectingSlopes: Bool) -> Set<Coordinate>? {
-//        
-//        let pathfinder = AStarPathfinder<Coordinate, Int> { currentState in
-//            let neighbors = currentState.neighbors(in: .cardinal)
-//                .filter { contents(at: $0) != nil && contents(at: $0) != .forest }
-//                .filter { respectingSlopes ? isRespectingSlope(from: currentState, to: $0) : true }
-//            
-//            return neighbors.map { .init(state: $0, cost: 1) }
-//        }
-//        
-//        return pathfinder.shortestPath(from: coordinate, toTargets: [to]) { state in
-//            return state.manhattanDistance(to: to)
-//        }.map { Set($0.states.dropFirst()) }
-//    }
-//}
-//
--- Day24/Python/d24_p2.py
+++ Day24/Python/d24_p2.py
@@ -1,26 +1,25 @@
 import sys
 from z3 import *
 
-D = open(sys.argv[1]).read().strip()
-L = D.split('\n')
+input = open(sys.argv[1]).read().strip()
+lines = input.split('\n')
 
-S = []
-for line in L:
+stones = []
+for line in lines:
   pos, vel = line.split('@')
   x,y,z = pos.split(', ')
   vx,vy,vz = vel.split(', ')
   x,y,z = int(x),int(y),int(z)
   vx,vy,vz = int(vx),int(vy),int(vz)
-  S.append((x,y,z,vx,vy,vz))
-
+  stones.append((x,y,z,vx,vy,vz))
 
 x,y,z,vx,vy,vz = Real('x'),Real('y'),Real('z'),Real('vx'),Real('vy'),Real('vz')
-T = [Real(f'T{i}') for i in range(len(S))]
+t = [Real(f't{i}') for i in range(len(stones))]
 SOLVE = Solver()
-for i in range(len(S)):
-  SOLVE.add(x + T[i]*vx - S[i][0] - T[i]*S[i][3] == 0)
-  SOLVE.add(y + T[i]*vy - S[i][1] - T[i]*S[i][4] == 0)
-  SOLVE.add(z + T[i]*vz - S[i][2] - T[i]*S[i][5] == 0)
+for i in range(len(stones)):
+  SOLVE.add(x + t[i]*vx - stones[i][0] - t[i]*stones[i][3] == 0)
+  SOLVE.add(y + t[i]*vy - stones[i][1] - t[i]*stones[i][4] == 0)
+  SOLVE.add(z + t[i]*vz - stones[i][2] - t[i]*stones[i][5] == 0)
 res = SOLVE.check()
 M = SOLVE.model()
 print(M.eval(x+y+z))
